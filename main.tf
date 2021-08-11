86720347475aef376406c1a4371bf1e5ffc1756d
Alex Egorov <alex-egorov-01@list.ru>
2021-08-11 16:43:18 +0300

    DVPS: add packer files

---
+++ main.tf
@@ -0,0 +1,61 @@
+provider "vsphere" {
+  user           = "intern@web-bee.loc"
+  password       = "88[pT,Jvdw#~H3=@"
+  vsphere_server = "192.168.15.34"
+ 
+  allow_unverified_ssl = true
+}
+
+data "vsphere_datacenter" "dc" {
+  name = "dc1"
+}
+ 
+data "vsphere_datastore" "datastore" {
+  name          = "ds1 - ssd esxi-test"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+ 
+data "vsphere_compute_cluster" "cluster" {
+  name          = "cluster1"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+ 
+data "vsphere_network" "network" {
+  name          = "VM Network"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+
+data "vsphere_virtual_machine" "template" {
+  name          = "Ubuntu-2004-Template"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+
+
+resource "vsphere_virtual_machine" "vm" {
+  name             = "Ubuntu_2004"
+  resource_pool_id = "${data.vsphere_compute_cluster.cluster.resource_pool_id}"
+  datastore_id     = "${data.vsphere_datastore.datastore.id}"
+
+  num_cpus = 2
+  memory   = 2048
+  guest_id = "${data.vsphere_virtual_machine.template.guest_id}"
+
+  scsi_type = "${data.vsphere_virtual_machine.template.scsi_type}"
+
+  network_interface {
+    network_id   = "${data.vsphere_network.network.id}"
+    adapter_type = "${data.vsphere_virtual_machine.template.network_interface_types[0]}"
+  }
+
+  disk {
+    label            = "disk1"
+    size             = "${data.vsphere_virtual_machine.template.disks.0.size}"
+    eagerly_scrub    = "${data.vsphere_virtual_machine.template.disks.0.eagerly_scrub}"
+    thin_provisioned = "${data.vsphere_virtual_machine.template.disks.0.thin_provisioned}"
+  }
+
+
+  clone {
+      template_uuid = "${data.vsphere_virtual_machine.template.id}"
+  }
+}
